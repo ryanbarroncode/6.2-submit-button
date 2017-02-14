aecbf319abbe4b79987cd40697942aa30abfb474
Ryan Barron <ryanbarron243@gmail.com>
2017-02-14 17:19:56 -0500

    first commit

---
+++ app/scripts/index.js
@@ -0,0 +1,34 @@
+var $ = require('jquery');
+// var Backbone = require('backbone');
+var models = require("./models/models.js");  //
+var pizzaCollection = require("./models/models.js").PizzaCollection;
+// var _ = require('underscore');
+var pizzaTemplate = require('../templates/pizza-list.hbs');
+
+
+var pizzas = new models.PizzaCollection();    //
+
+console.log(pizzas);
+$('button').on('click', function(){
+$('button').text('Loading');
+
+
+pizzas.fetch();
+})
+
+pizzas.on('update', function(){
+  console.log(pizzas);
+  pizzas.forEach(function(pizza){
+    $('button').text('Submit');
+
+    pizza.get('name');
+
+    $('#pizza-menu').append(pizzaTemplate(pizza.toJSON()));
+
+  });
+
+});
+
+//anonymous function expression to loop pizza
+
+console.log(pizzas.toJSON());
---
+++ app/scripts/models/models.js
@@ -0,0 +1,23 @@
+var Backbone = require('backbone');
+
+
+
+// var button = Backbone.Model.extend({
+//
+// });
+
+// var PizzaCollection = require('./models/models.js').Pizzaollection;
+var Pizza = Backbone.Model.extend({
+
+});
+
+var PizzaCollection = Backbone.Collection.extend({
+  model: Pizza,
+  url: 'http://tiny-lasagna-server.herokuapp.com/collections/pizza',
+
+});console.log(PizzaCollection);
+
+module.exports = {
+  PizzaCollection: PizzaCollection,
+  Pizza: Pizza
+};
